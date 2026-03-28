d7ac07a4abc872b42437e5448a93f49724896936
GowthamJayakumar016 <gowthamjayakumar016@gmail.com>
2026-03-28 10:57:42 +0530

    files added

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HackathanChecker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
